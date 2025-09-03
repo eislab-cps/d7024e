313ff5afca1ce07e860b5594ded44e6c9582b87e
Johan <jsbollll@gmail.com>
2025-09-03 13:30:52 +0200

    add docker container building

---
+++ build.bat
@@ -0,0 +1,12 @@
+@echo off
+cd labs
+
+set GOARCH=amd64
+SET GOOS=linux
+set CGO_ENABLED=0
+echo Building Go project...
+go build -o ./kademlia.exe
+echo Docker stuff...
+docker build . -t kadlab
+
+cd ..
