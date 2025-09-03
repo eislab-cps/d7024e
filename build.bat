2f1bd17c9316ca5ea3317e74e60af2ef6bf6a73f
Johan <jsbollll@gmail.com>
2025-09-03 13:46:38 +0200

    only build docker if go succeed

--- build.bat
+++ build.bat
@@ -5,8 +5,6 @@ set GOARCH=amd64
 SET GOOS=linux
 set CGO_ENABLED=0
 echo Building Go project...
-go build -o ./kademlia.exe
-echo Docker stuff...
-docker build . -t kadlab
+go build -o ./kademlia.exe && echo Docker stuff... && docker build . -t kadlab
 
 cd ..
